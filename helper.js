f988d4bec7af84664055865a28121fdf4e1bc7eb
Srdja Dzogaz <84318757+srdjadzogaz@users.noreply.github.com>
2024-04-16 15:40:18 +0200

    Move functions to helper and environment handle (#3)
    * Move functions to helper.js and handle environment

--- helper.js
+++ helper.js
@@ -1,18 +1,52 @@
-const fs = require('fs');
+const fs = require("fs");
+const path = require("path");
+const base64 = require("base-64");
+
+function getEnvPath(environment) {
+  const usersFilePath = path.join(__dirname, `users_${environment}.json`);
+  return usersFilePath;
+}
+
+function checkBasicAuthFromRequest(request) {
+  if (!request.headers || !request.headers.authorization) {
+    console.log("Request headers not passed");
+    return false;
+  }
+
+  const authHeader = request.headers.authorization;
+  if (!authHeader.startsWith("Basic ")) {
+    console.log("Basic authorization is missing");
+    return false;
+  }
+
+  const credentials = base64.decode(authHeader.substring(6));
+  const [username, password] = credentials.split(":");
+
+  const usersFilePath = path.join(__dirname, "users_basic_auth.json");
+  const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
+
+  for (const user of users) {
+    if (user.username === username && user.password === password) {
+      console.log("User authorized to perform an action");
+      return true;
+    }
+  }
+  return false;
+}
 
 function login_change(username, loggedIn) {
-  const users = JSON.parse(fs.readFileSync('users_basic_auth.json', 'utf8'));
-  console.log(username)
-  const user = users.find(user => user.username === username);
+  const users = JSON.parse(fs.readFileSync("users_basic_auth.json", "utf8"));
+  console.log(username);
+  const user = users.find((user) => user.username === username);
 
   if (!user) {
-    throw new Error('Username not found');
+    throw new Error("Username not found");
   }
 
   // Update the loggedIn status for the user
   user.loggedIn = loggedIn;
 
-  fs.writeFileSync('users_basic_auth.json', JSON.stringify(users, null, 2));
+  fs.writeFileSync("users_basic_auth.json", JSON.stringify(users, null, 2));
 
   console.log(`Logged in status updated for user ${username}`);
 }
@@ -20,10 +54,12 @@ function login_change(username, loggedIn) {
 // Read the users_basic_auth.json file and return its contents
 function getUsersCredentials() {
   try {
-    const usersCredentials = JSON.parse(fs.readFileSync('users_basic_auth.json', 'utf8'));
+    const usersCredentials = JSON.parse(
+      fs.readFileSync("users_basic_auth.json", "utf8"),
+    );
     return usersCredentials;
   } catch (error) {
-    console.error('Error reading users_basic_auth.json:', error.message);
+    console.error("Error reading users_basic_auth.json:", error.message);
     return [];
   }
 }
@@ -32,26 +68,43 @@ function getUsersCredentials() {
 function writeUsersToFile(usersFilePath) {
   try {
     const usersData = JSON.stringify(users, null, 2);
-    fs.writeFileSync(usersFilePath, usersData, 'utf8');
+    fs.writeFileSync(usersFilePath, usersData, "utf8");
   } catch (error) {
-    console.error(`Error writing users file for ${environment} environment:`, error.message);
+    console.error(
+      `Error writing users file for ${usersFilePath} environment:`,
+      error.message,
+    );
   }
 }
 
-function readUsersFromFile(usersFilePath) {
-try {
-  const usersData = fs.readFileSync(usersFilePath, 'utf8');
-  users = JSON.parse(usersData);
-  return users;
-} catch (error) {
-  console.error(`Error reading users file for ${environment} environment:`, error.message);
-  return error(`Error reading users file for ${environment} environment:`, error.message)
+function readUsersFromFile(usersFilePath, environment) {
+  try {
+    const usersData = fs.readFileSync(usersFilePath, "utf8");
+    users = JSON.parse(usersData);
+    return users;
+  } catch (error) {
+    console.error(
+      `Error reading users file for ${environment} environment:`,
+      error.message,
+    );
+    return error(
+      `Error reading users file for ${environment} environment:`,
+      error.message,
+    );
+  }
 }
+
+// Function to generate a unique ID for a new user
+function generateUniqueId() {
+  return users.length > 0 ? Math.max(...users.map((user) => user.id)) + 1 : 1;
 }
 
 module.exports = {
+  getEnvPath,
   login_change,
   getUsersCredentials,
   writeUsersToFile,
-  readUsersFromFile
-} 
+  readUsersFromFile,
+  checkBasicAuthFromRequest,
+  generateUniqueId,
+};
