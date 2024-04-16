3ccc4e9b8c806530cbd72942f19e27a5d7bbe34c
Srdja Dzogaz <dzogaz@work.co>
2024-04-16 14:51:15 +0200

    Fix put and patch methods

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.put("/users/:id", (req, res) => {
 
   if (index !== -1 && updatedUser && updatedUser.name && updatedUser.age) {
     users[index] = { ...users[index], ...updatedUser };
-    writeUsersToFile();
+    writeUsersToFile(usersFilePath, environment);
     res.status(200).json(users[index]);
   } else {
     res.status(400).json({ message: "Invalid user data or user not found" });
@@ -116,7 +116,7 @@ app.patch("/users/:id", (req, res) => {
 
   if (index !== -1 && updatedFields) {
     users[index] = { ...users[index], ...updatedFields };
-    writeUsersToFile(); // Update the users file
+    writeUsersToFile(usersFilePath, environment); // Update the users file
     res.status(200).json(users[index]);
   } else {
     res.status(400).json({ message: "Invalid user data or user not found" });
@@ -137,7 +137,7 @@ app.delete("/users/:id", (req, res) => {
 
   if (index !== -1) {
     const deletedUser = users.splice(index, 1);
-    writeUsersToFile(usersFilePath); // Update the users file
+    writeUsersToFile(usersFilePath, environment); // Update the users file
     res.status(200).json(deletedUser[0]);
   } else {
     res.status(400).json({ message: "User not found" });
